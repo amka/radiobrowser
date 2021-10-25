a76cc540e521f44809e4dea2d3073b01803745c0
Andrey Maksimov <andrey@tenderowl.com>
2021-10-25 21:25:10 +0300

    Add CountryCode model

---
+++ lib/src/models/country_code.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'country_code.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CountryCode _$CountryCodeFromJson(Map<String, dynamic> json) => CountryCode(
+      json['name'] as String,
+      json['stationcount'] as int,
+    );
+
+Map<String, dynamic> _$CountryCodeToJson(CountryCode instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'stationcount': instance.stationCount,
+    };
